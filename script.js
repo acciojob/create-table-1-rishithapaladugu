087932e740a083b790ab36cd3b89d652620ee03c
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-08-13 12:13:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
+	let table = document.getElementById("sampleTable");
+	let newRow = table.insertRow(0);
+	let cell1 = newRow.insertCell(0);
+	let cell2 = newRow.insertCell(1);
+	let innerhtml = "New Cell1";
+	let innerHtml = "New Cell2";
   
   
 }
